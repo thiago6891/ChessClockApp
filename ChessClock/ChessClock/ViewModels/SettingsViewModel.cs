7c77d879dca0faf203632a4f8246e90a92352844
Thiago Cordeiro <thiago6891@gmail.com>
2018-08-30 09:30:03 -0300

    Removed unused usings. close #8

--- ChessClock/ChessClock/ViewModels/SettingsViewModel.cs
+++ ChessClock/ChessClock/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace ChessClock
--- ChessClock/ChessClock/Views/SettingsView.xaml.cs
+++ ChessClock/ChessClock/Views/SettingsView.xaml.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ChessClock
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SettingsView : ContentPage
 	{
 		public SettingsView ()
